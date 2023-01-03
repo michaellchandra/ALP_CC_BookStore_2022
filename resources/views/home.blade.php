31555e991287fc853072221c6c3d176a0aff4ce6
UnknownArt01 <84028978+UnknownArt01@users.noreply.github.com>
2023-01-03 17:59:31 +0700

    login register

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,23 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 @section('content')
-<header class="pt-5">
-    
-    <div class="container pt-4 pt-xl-5">
-        <div class="row pt-5">
-            <div class="col-md-8 text-center text-md-start mx-auto">
-                <div class="text-center">
-                    <h1 class="display-4 fw-bold mb-5">Cari Buku Cepat, Mudah dan&nbsp;<span class="underline">Lengkap</span>.</h1>
-                    <p class="fs-5 text-muted mb-5">Cari buku dari mana saja, kapan saja dengan real-time</p>
-                    <a class="btn btn-warning shadow" role="button" href="dokumentasi">Lihat Dokumentasi</a>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
                 </div>
-            </div>
-            <div class="col-12 col-lg-10 mx-auto">
-                <div class="text-center position-relative"><img class="img-fluid" src="assets/img/illustrations/meeting.svg" style="width: 800px;"></div>
             </div>
         </div>
     </div>
-</header>
+</div>
 @endsection
